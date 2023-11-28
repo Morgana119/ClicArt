b4be73cebf3a01adb7505181584b4e5a4dc2ff57
Morgana119 <145613786+Morgana119@users.noreply.github.com>
2023-11-28 17:47:42 -0600

    Update Gato.h

--- Gato.h
+++ Gato.h
@@ -1,3 +1,4 @@
+//Subclase Gato, relacion de herencia con Animal 
 #ifndef GATO_H
 #define GATO_H
 
@@ -8,15 +9,23 @@ class Gato : public Animal{
         int acariciar;
 
     public:
-        Gato(){}
+        Gato();
+        Gato(int,string,int);
         string getAcariciar();
         void setAcariciar(int);
 };
 
-//Gato::Gato(){} -> Por qué causa error?
-string Gato::getAcariciar(){
+Gato::Gato(){ //Constructor, iniciliza valor de puntaje
+    puntaje = 0;
+} 
+Gato::Gato(int _acariciar,string _nombre,int _puntaje){ //Contructor con atributos de herencia
+    acariciar = _acariciar;
+    nombre = _nombre;
+    puntaje = 0;
+}
+string Gato::getAcariciar(){ //Método de cariño diferente para objeto tipo Gato
     string mensajeAcariciar;
-    if (acariciar == 2){
+    if (acariciar == 2){ //De acuerdo a la variable respuestaCarino, si es objeto tipo Gato, se le asginan 25 puntos
         mensajeAcariciar = "\n Acariciaste a tu gato \n +25 puntos";
     }
     return mensajeAcariciar;
