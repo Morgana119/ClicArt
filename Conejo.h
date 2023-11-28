08a175ef5625c5a1017fc2af135e2d79a02d7385
Morgana119 <145613786+Morgana119@users.noreply.github.com>
2023-11-28 17:48:08 -0600

    Update Conejo.h

--- Conejo.h
+++ Conejo.h
@@ -1,3 +1,4 @@
+//Subclase Conejo, relacion de herencia con Animal 
 #ifndef CONEJO_H
 #define CONEJO_H
 
@@ -5,16 +6,23 @@
 
 class Conejo : public Animal{
     public:
-        Conejo(){}
+        Conejo();
+        Conejo(string,int);
         string getLimpieza();
         void setLimpieza(int);
 };
 
-//Conejo::Conejo(){} -> Por qué causa error?
-string Conejo::getLimpieza(){
+Conejo::Conejo(){ //Constructor, iniciliza valor de puntaje
+    puntaje = 0;
+}
+Conejo::Conejo(string _nombre,int _puntaje){ //Contructor con atributos de herencia
+    nombre = _nombre;
+    puntaje = 0;
+}
+string Conejo::getLimpieza(){ //Método de limpieza diferente para objeto tipo Conejo
     string mensajeLimpieza;
-    if (limpieza == 4){
-        mensajeLimpieza = "\n Limpiaste a tu conejo \n +20 puntos";
+    if (limpieza == 4){ //De acuerdo a la variable respuestaCuidados == 4
+        mensajeLimpieza = "\n Limpiaste a tu conejo \n +20 puntos"; //Solo si es objeto tipo Conejo se le dan 20 puntos
     }
     return mensajeLimpieza;
 }
