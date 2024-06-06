80ff6e4baa89425abf7e1a09168bbd9ded6c911c
Morgana119 <145613786+Morgana119@users.noreply.github.com>
2024-06-06 10:26:11 -0600

    Correcciones

--- Cubism.h
+++ Cubism.h
@@ -55,11 +55,13 @@ void Cubism::setAbstraction(string abstract){
 string Cubism::infoArt(){
     stringstream aux;
     if (technic == ""){
-    aux << endl << "Titulo: "<< tittle << endl << "Artista: "<< artist << endl << "Ano: "<< year << endl << "Abstraccion: " << abstraction << endl;
+    aux << endl << "Titulo: "<< tittle << endl << "Artista: "<< artist << endl << "Ano: "<< year << endl;
+    aux << "Abstraccion: " << abstraction << endl;
         return aux.str();
     }
     else{
-    aux << endl << "Titulo: "<< tittle << endl << "Artista: "<< artist << endl << "Ano: "<< year << endl << "Tecnica: " << technic << endl << "Abstraccion: " << abstraction << endl;
+    aux << endl << "Titulo: "<< tittle << endl << "Artista: "<< artist << endl << "Ano: "<< year << endl;
+    aux << "Tecnica: " << technic << endl << "Abstraccion: " << abstraction << endl;
         return aux.str();
     }
 }
